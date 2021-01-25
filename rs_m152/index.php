18adb52d42b37ae8703a9d6dff43ddde640e7499
AlvesL <63236513+alvsg@users.noreply.github.com>
2021-01-25 09:00:42 +0100

    Add files via upload

---
+++ rs_m152/index.php
@@ -0,0 +1,25 @@
+<!--
+    PROJET : rs_M152
+    AUTEUR : ALVES GUASTTI Letitia (I.FA-P3A)
+    DESC : 
+    VERSION : 25.01.2021 v1.0
+-->
+
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Blog</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+
+</body>
+
+</html>
